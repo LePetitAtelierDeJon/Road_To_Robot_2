baa9e571070947441eac681a510586268561ed4a
Jonathan Bihet <lepetitatelierdejon@gmail.com>
2022-07-20 19:52:10 +0200

    refacto + add thickness on drawing

--- sonar_gui/src/main.rs
+++ sonar_gui/src/main.rs
@@ -1,7 +1,6 @@
-// use std::{thread, time};
+use std::{thread, time};
 
 use sfml::graphics::*;
-// use sfml::system::*;
 use sfml::window::*;
 
 mod radar_file;
@@ -15,8 +14,8 @@ fn main() {
 
     let mut radar = Radar::default();
 
-    // let mut angle: i16 = 0;
-    // let mut orientation: i16 = 1;
+    let mut angle: i16 = 0;
+    let mut orientation: i16 = 1;
 
     while window.is_open() {
         while let Some(event) = window.poll_event() {
@@ -27,32 +26,20 @@ fn main() {
 
         window.clear(Color::rgb(30, 30, 30));
 
-        radar.set_current_radar_orientation(45, 50.0);
+        if angle >= 180 {
+            orientation = -1;
+        } else if angle <= 0 {
+            orientation = 1;
+        }
 
-        // radar.draw(&window);
-        // radar.draw_radar_direction_line(&window, angle);
-        // radar.draw_echo(&window, obstacle_point);
-
-        // if angle >= 180 {
-        //     orientation = -1;
-        // } else if angle <= 0 {
-        //     orientation = 1;
-        // }
-
-        // angle += orientation;
-
-        // window.display();
-
-        radar.set_current_radar_orientation(1, 40.0);
-        radar.set_current_radar_orientation(125, 90.0);
-        radar.set_current_radar_orientation(145, 95.0);
-        radar.set_current_radar_orientation(179, 1.0);
-        radar.set_current_radar_orientation(0, 100.0);
+        angle += orientation;
+       
+        radar.set_current_radar_orientation(angle, -1.0);        
 
         radar.draw(&window);
         window.display();
 
-        // let sleep_duration = time::Duration::from_millis(10);
-        // thread::sleep(sleep_duration);
+        let sleep_duration = time::Duration::from_millis(10);
+        thread::sleep(sleep_duration);
     }
 }
--- sonar_gui/src/radar_file.rs
+++ sonar_gui/src/radar_file.rs
@@ -13,7 +13,8 @@ pub struct Radar {
     pub position: Vector2f,
     pub detection_angle: u16,
     current_radar_angle: i16,
-    echo_map: HashMap<i16, f32>
+    echo_map: HashMap<i16, f32>,
+    border_thickness: u16,
 }
 
 impl Radar {
@@ -25,48 +26,81 @@ impl Radar {
             detection_angle: 180,
             echo_color: Color::rgb(0, 250, 100),
             frame_color: Color::rgb(250, 250, 250),
-            frame_nb_point: 30,
+            frame_nb_point: 60,
             echo_precision: 4,
             echo_radius: 3.0,
             current_radar_angle: 0,
-            echo_map: HashMap::new()
+            echo_map: HashMap::new(),
+            border_thickness: 3,
         }
     }
-    
     pub fn draw(&self, window: &RenderWindow) {
         self.draw_frame(window);
         self.draw_radar_direction_line(window);
         self.draw_echo(window);
-        
     }
 
     fn draw_frame(&self, window: &RenderWindow) {
         let mut vec = Vec::new();
         let angle_fraction = (self.detection_angle / self.frame_nb_point) as i32;
-        vec.push(Vertex::with_pos_color(self.position, self.frame_color));
+        let new_pos = Vector2f::new(
+            self.position.x,
+            self.position.y + self.border_thickness as f32,
+        );
+        vec.push(Vertex::with_pos_color(new_pos, self.frame_color));
+        for i in 0..(self.frame_nb_point + 1) as i32 {
+            let f = -(i * angle_fraction) as f32;
+            let x =
+                new_pos.x + (self.radius + self.border_thickness * 2) as f32 * f.to_radians().cos();
+            let y =
+                new_pos.y + (self.radius + self.border_thickness * 2) as f32 * f.to_radians().sin();
+            let p = Vector2f::new(x, y);
+            vec.push(Vertex::with_pos_color(p, self.frame_color));
+        }
+        vec.push(Vertex::with_pos_color(new_pos, self.frame_color));
+        window.draw_primitives(
+            &vec[..],
+            PrimitiveType::TRIANGLE_FAN,
+            &RenderStates::DEFAULT,
+        );
+
+        vec.clear();
+        vec.push(Vertex::with_pos_color(
+            self.position,
+            Color::rgb(30, 30, 30),
+        ));
         for i in 0..(self.frame_nb_point + 1) as i32 {
             let f = -(i * angle_fraction) as f32;
             let x = self.position.x + (self.radius) as f32 * f.to_radians().cos();
             let y = self.position.y + (self.radius) as f32 * f.to_radians().sin();
             let p = Vector2f::new(x, y);
-            vec.push(Vertex::with_pos_color(p, self.frame_color));
+            vec.push(Vertex::with_pos_color(p, Color::rgb(30, 30, 30)));
         }
-        vec.push(Vertex::with_pos_color(self.position, self.frame_color));
-        window.draw_primitives(&vec[..], PrimitiveType::LINE_STRIP, &RenderStates::DEFAULT);
+        vec.push(Vertex::with_pos_color(
+            self.position,
+            Color::rgb(30, 30, 30),
+        ));
+        window.draw_primitives(
+            &vec[..],
+            PrimitiveType::TRIANGLE_FAN,
+            &RenderStates::DEFAULT,
+        );
     }
-    
     pub fn draw_radar_direction_line(&self, window: &RenderWindow) {
-        let mut vec = Vec::new();
-        vec.push(Vertex::with_pos_color(self.position, self.echo_color));
+        let mut line = RectangleShape::with_size(Vector2f::new(            
+            self.radius as f32,
+            self.border_thickness as f32,
+        ));
+
+        line.set_position(self.position);
         let f = -(self.current_radar_angle as f32);
-        let x = self.position.x + (self.radius) as f32 * f.to_radians().cos();
-        let y = self.position.y + (self.radius) as f32 * f.to_radians().sin();
-        let p = Vector2f::new(x, y);
-        vec.push(Vertex::with_pos_color(p, self.echo_color));
-        window.draw_primitives(&vec[..], PrimitiveType::LINE_STRIP, &RenderStates::DEFAULT);
+        line.set_rotation(f);
+        line.set_fill_color(self.echo_color);
+
+        window.draw_rectangle_shape(&line, &RenderStates::DEFAULT);
     }
 
-    pub fn draw_echo(&self, window: &RenderWindow) {    
+    pub fn draw_echo(&self, window: &RenderWindow) {
         for (angle, distance) in self.echo_map.iter() {
             let draw_distance = (distance * self.radius as f32) / self.scan_distance;
             let f = -angle;
@@ -75,7 +109,7 @@ impl Radar {
             let point = Vector2f::new(x, y);
             let mut circle = CircleShape::new(self.echo_radius, self.echo_precision);
             circle.set_position(point);
-            circle.set_origin(Vector2f::new(self.echo_radius,self.echo_radius));
+            circle.set_origin(Vector2f::new(self.echo_radius, self.echo_radius));
             circle.set_fill_color(self.echo_color);
             window.draw_circle_shape(&circle, &RenderStates::default());
         }
@@ -83,6 +117,8 @@ impl Radar {
 
     pub fn set_current_radar_orientation(&mut self, angle: i16, echo: f32) {
         self.current_radar_angle = angle;
-        self.echo_map.insert(angle, echo);
+        if echo > -1.0 {
+            self.echo_map.insert(angle, echo);
+        }
     }
 }
